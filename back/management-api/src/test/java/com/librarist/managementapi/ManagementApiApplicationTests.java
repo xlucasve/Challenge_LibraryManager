3a600114b798c4188a1605df5e55f1cbc8899cf9
Lucas <lucasvonelm@gmail.com>
2024-03-13 13:46:40 -0300

    Initial backend commit

---
+++ back/management-api/src/test/java/com/librarist/managementapi/ManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarist.managementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
